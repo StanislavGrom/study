d1b5511c68c6b87f05ee22c2103bc5b1dedbbf3d
Stas Grom <sgrom@magecom.us>
2022-07-03 19:05:42 +0300

    test-branch list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import List from "./components/list/List";
+
+const data = [
+    {
+        "customer_group_id": 0,
+        "customer_group_code": "NOT LOGGED IN",
+        "tax_class_id": 3
+    },
+    {
+        "customer_group_id": 1,
+        "customer_group_code": "Retail",
+        "tax_class_id": 3
+    },
+    {
+        "customer_group_id": 2,
+        "customer_group_code": "WS - Active",
+        "tax_class_id": 5
+    },
+    {
+        "customer_group_id": 5,
+        "customer_group_code": "WS - Sales Rep",
+        "tax_class_id": 5
+    },
+    {
+        "customer_group_id": 6,
+        "customer_group_code": "WS - Pending",
+        "tax_class_id": 5
+    },
+    {
+        "customer_group_id": 7,
+        "customer_group_code": "TEST Retail Internal",
+        "tax_class_id": 3
+    }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <List data={data}/>
       </header>
     </div>
   );
